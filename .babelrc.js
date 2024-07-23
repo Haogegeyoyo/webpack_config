439c028664e2101513baef6d35888fa6e4a0348a
haoge <1468853309@qq.com>
2024-07-23 11:07:32 +0800

    up ： 增加 eslint 与 babel 配置

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   presets :[ [
       "@babel/preset-env",// 智能预设，编译 ES6的语法
-      {
-        useBuiltIns : 'usage', // 按需引入
-        corejs : 3 // 使用版本为3 的 core-js
-      }
-    ]  
+      // {
+      //   useBuiltIns : 'usage', // 按需引入
+      //   corejs : 3 // 使用版本为3 的 core-js
+      // }
+      { "modules": false } //模块的处理交由webpack,配合tree-shaking
+    ],
+    "@babel/preset-react" // react jsx语法
+
+    
   ]
 }
--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,29 @@
 module.exports={
+
+  env: {
+    browser: true,
+    es6: true,
+  },
+
+  // parser解构器
+  parser : "babel-eslint",
+
+  plugins: ['react', 'react-hooks',"jsx-a11y"],
+    
+  //继承规则
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+
   // 解析选项
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
 
-  },
-  
-  //具体检查规则
-  rules :{},
-  
-  //继承规则
-  extends :[] ,
-  env :{
-    es6 : true,
-  },
-  parserOptions:{ sourceType : "module"},
-  
-  // parser解构器
-  parser : "@babel/eslint-parser"
+   //具体检查规则
+   rules: {
+    'no-unused-vars': 0, // 允许存在未引用的变量
+   },
 }
